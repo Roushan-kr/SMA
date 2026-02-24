48789af39a91a954727b7eed79eafd1820284b81
Roushan kr <kumar7roushanjha2005@gmail.com>
2026-02-24 22:56:03 +0530

    update pkg from ppg to pg

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,7 @@
-import { PrismaClient } from "../generated/prisma/client.js";
-import { PrismaPostgresAdapter } from '@prisma/adapter-ppg'
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/client.js'
 
 const DATABASE_URL = process.env["DATABASE_URL"] as string;
-const adapter = new PrismaPostgresAdapter({ connectionString: DATABASE_URL })
+const adapter = new PrismaPg({ connectionString: DATABASE_URL })
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
-
-if (process.env["NODE_ENV"] !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+export const prisma = new PrismaClient({ adapter });
